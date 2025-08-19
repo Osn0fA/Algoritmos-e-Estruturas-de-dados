04a401f91258ea5a73a2444c54077edfd7ed6171
Afonso <afonsobranco34@outlook.com>
2025-08-18 21:44:05 -0300

    asd

--- Aula-1/Deed.c
+++ Aula-1/Deed.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
 int main(void){
+
+    /*
+    SCAN - Get Data! (RAM)-------------------------------->
+
+    int idade = 0;
+    scanf("%d",&idade);
     
-    
+    ONE LETTER-------------------------------------->
+
+        char letra = getchar();
+
+    A WHOLE SENTECE-------------------------------------->
+
+        char = var1[50];
+        fgets(var1, size of (var1), stdin)
+    */
+
+    char nome[30];
+    char resposta = 'a';
+
+    fgets(nome, 30, stdin);
+
+    printf("Hello %s", nome);
+
+    printf("Você tem medo do escuro? Y/N \n Resposta = ");
+
+    scanf("%c",resposta);
 
 }
