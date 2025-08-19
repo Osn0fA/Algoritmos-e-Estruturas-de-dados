f04f799229127714c06e363019fe97ba38a77f0b
Afonso <afonsobranco34@outlook.com>
2025-08-18 22:05:39 -0300

    sexo selvagem com sua mãe

---
+++ Contra File.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 10, y = 4;
+
+    printf("\n\n ### Meu Progama ###\n\n");
+    printf("-> A soma .......................: %i\n", x + y);
+    printf("-. O resto da divisão.: %i\n",x%y);
+
+    return 0;
+
+}
---
+++ Contra baixo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 0;
+
+    printf("Menu...");
+    printf("\nSoma Basica...");
+
+    x += 1;
+
+    printf("\n%d",x);
+}
