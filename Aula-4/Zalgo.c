6dc8e2251440dd2596db9b96ddaf1ee77008711f
Afonso <afonsobranco34@outlook.com>
2025-09-03 21:11:21 -0300

    kas

--- Aula-4/Zalgo.c
+++ Aula-4/Zalgo.c
@@ -1,8 +1,11 @@
-#include <string.h>
 #include <stdio.h>
 
 int main(){
 
+int array[8] = {'1','2','3','4','5','6','7','8'};
 
-    
+printf("%p\n",array);
+printf("%p\n",&array[0]);
+printf("%d\n",*array);
+
 }
