c3c3b43c9bab789dbbd28a048fa4405de9a02670
Afonso <afonsobranco34@outlook.com>
2025-09-22 22:01:31 -0300

    Update Zalgo-3.c

--- Aula-4/Zalgo-3.c
+++ Aula-4/Zalgo-3.c
@@ -5,6 +5,7 @@ int main(){
 int array[4] = {'1','2','3','4'};
 *array = 7;
 *(array +1) = 14;
+int notas = 0;
 
 
 for (int i = 0; i < 4;i++){
@@ -12,5 +13,7 @@ for (int i = 0; i < 4;i++){
     printf("%d\n",*(array + i));
 
 }
+
+free(notas);
 
 }
